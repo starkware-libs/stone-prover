a28e55e510b842b79af0267b4b43367d0d2651fd
Noa Moulin <noa.moulin42@gmail.com>
2024-02-01 13:43:16 +0100

    added yun pacman and apk support to install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,17 +1,94 @@
 #!/bin/bash
 
+#find the package manager
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/debian_version]=apt
+osInfo[/etc/alpine-release]=apk
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        package_manager=${osInfo[$f]}
+    fi
+done
+
+
 # Show executed shell commands
 set -o xtrace
 # Exit on error.
 set -e
 
-apt update
-apt install -y python3.9-dev git wget gnupg2 elfutils libdw-dev python3-pip libgmp3-dev
 
-pip install cpplint pytest numpy
-pip install cmake
+with_apt() {
+    apt update
+    apt install -y python3.9-dev git wget gnupg2 elfutils libdw-dev python3-pip libgmp3-dev
 
-apt install -y clang-12 clang-format-12 clang-tidy-6.0 libclang-12-dev llvm-12
+    pip install cpplint pytest numpy
+    pip install cmake
+
+    apt install -y clang-12 clang-format-12 clang-tidy-6.0 libclang-12-dev llvm-12
+}
+
+with_yum() {
+    yum update
+    yum install -y python39-devel git wget gnupg2 elfutils-libelf-devel python3-pip gmp-devel
+
+    pip install cpplint pytest numpy
+    pip install cmake
+
+    yum install -y clang clang-format clang-tools-extra llvm-devel
+}
+
+with_dnf() {
+    dnf update
+    dnf install -y python39-devel git wget gnupg2 elfutils-libelf-devel python3-pip gmp-devel
+
+    pip install cpplint pytest numpy
+    pip install cmake
+
+    dnf install -y clang clang-format clang-tools-extra llvm-devel
+}
+
+with_apk() {
+
+apk update
+
+apk add python3-dev git wget gnupg elfutils-dev dw-dev py3-pip gmp-dev clang clang-dev llvm llvm-dev clang-tools clang-extra-tools
+
+pip install cpplint pytest numpy cmake
+
+}
+
+with_pacman() {
+    pacman -Syu --noconfirm
+    pacman -S --noconfirm python39 git wget gnupg elfutils libdw python-pip gmp
+
+    pip install cpplint pytest numpy
+    pip install cmake
+
+    pacman -S --noconfirm clang clang-format clang-tools-extra llvm
+}
+
+case $package_manager in
+  yum)
+    with_yum
+    ;;
+  pacman)
+    with_pacman
+    ;;
+  apt)
+    with_apt
+    ;;
+  apk)
+    with_apk
+    ;;
+  *)
+    echo "no package manager found"
+    exit 1
+    ;;
+esac
 
 ln -sf /usr/bin/clang++-12 /usr/bin/clang++
 ln -sf /usr/bin/clang-12 /usr/bin/clang
