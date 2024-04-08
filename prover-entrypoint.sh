73a45c85899ce53be39b6cec75c04d4656b4295a
Neo <neotheprogramist@proton.me>
2024-04-08 10:07:59 +0000

    remove equal sign

--- prover-entrypoint.sh
+++ prover-entrypoint.sh
@@ -15,11 +15,11 @@ cairo-run \
     2>&1 > /dev/null && \
 
 cpu_air_prover \
-    --out_file=program_proof.json \
-    --private_input_file=program_private_input.json \
-    --public_input_file=program_public_input.json \
-    --prover_config_file=cpu_air_prover_config.json \
-    --parameter_file=cpu_air_params.json \
+    --out_file program_proof.json \
+    --private_input_file program_private_input.json \
+    --public_input_file program_public_input.json \
+    --prover_config_file cpu_air_prover_config.json \
+    --parameter_file cpu_air_params.json \
     -generate_annotations && \
 
 cat program_proof.json
