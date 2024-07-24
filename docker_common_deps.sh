3ab102ec8af0c45248b3b6a567815918d90ee681
257Byte <257byte@gmail.com>
2024-07-24 14:06:47 +0300

    Migrate to Bazel 7.2.1

--- docker_common_deps.sh
+++ docker_common_deps.sh
@@ -1,4 +1,4 @@
-curl -L -o /tmp/bazel_install.sh https://github.com/bazelbuild/bazel/releases/download/5.4.0/bazel-5.4.0-installer-linux-x86_64.sh
+curl -L -o /tmp/bazel_install.sh https://github.com/bazelbuild/bazel/releases/download/7.2.1/bazel-7.2.1-installer-linux-x86_64.sh
 chmod +x /tmp/bazel_install.sh
 /tmp/bazel_install.sh
 
